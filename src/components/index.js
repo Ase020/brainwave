24d320470947d40a23d13cd25b6683688dfa86e2
felixNyalenda <felixasejunior@gmail.com>
2024-03-07 10:29:54 +0300

    Hero section

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { default as Button } from "./Button";
+export { default as CompanyLogos } from "./CompanyLogos";
+export { default as Generating } from "./Generating";
 export { default as Header } from "./Header";
 export { default as Hero } from "./Hero";
+export { default as Notification } from "./Notification";
+export { default as Section } from "./Section";
