f19f5391d4f606a2ab9ed9fc94aabb60350103fb
felixNyalenda <felixasejunior@gmail.com>
2024-03-08 13:07:40 +0300

    bento box

--- src/components/index.js
+++ src/components/index.js
@@ -8,3 +8,4 @@ export { default as Heading } from "./Heading";
 export { default as Hero } from "./Hero";
 export { default as Notification } from "./Notification";
 export { default as Section } from "./Section";
+export { default as Services } from "./Services";
